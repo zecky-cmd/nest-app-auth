92ecccf0b2d2273b882bd1d35ec05fab08c28032
EzéchielDev <yaoezchiel@gmail.com>
2026-03-05 22:30:48 +0000

    mise en place des dto pour le module auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { DatabaseService } from './database/database.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
+  imports: [DatabaseModule, UsersModule, AuthModule],
   controllers: [AppController, DatabaseController, UsersController],
   providers: [AppService, DatabaseService],
 })
